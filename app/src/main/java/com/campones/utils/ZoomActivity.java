5f52ae9f90c45125b90f77b269c7e940e4eb1436
jonatasmcampos <camposjonatas3@gmail.com>
2022-04-12 16:32:29 -0300

    zoom activity btn e links ok

--- app/src/main/java/com/campones/utils/ZoomActivity.java
+++ app/src/main/java/com/campones/utils/ZoomActivity.java
@@ -12,6 +12,13 @@ public class ZoomActivity extends AppCompatActivity {
     private String urlCapacitacao = "https://success.zoom.us/rec/play/N-ZTeZ1ilIFb0vqaeawDxCg99SojdzW9XtBjg4vSVV2YxHZbwP-0JooL97QTuTcsIsHEe_txT5VnHipn.uZIXuCb0UZPSCU4l?continueMode=true&_x_zm_rtaid=XyNfc8dlQYC1uG0n6uSWtg.1628942468309.b32c2f40a6c107105bb5b72154caa06d&_x_zm_rhtaid=734";
     private String urlCentralAjuda = "https://support.zoom.us/hc/pt-br?_ga=2.142082673.210812303.1628946026-1779077322.1628946026";
 
+    private String linkCriar = "https://support.zoom.us/hc/pt-br/articles/201362613-Como-realizo-uma-reuni%C3%A3o-por-v%C3%ADdeo-";
+    private String linkAgendar = "https://support.zoom.us/hc/pt-br/articles/201362413-Agendamento-de-reuni%C3%B5es";
+    private String linkIngressar = "https://support.zoom.us/hc/pt-br/articles/201362193-Como-ingressar-em-uma-reuni%C3%A3o";
+    private String linkGravar = "https://support.zoom.us/hc/pt-br/articles/201362473-Grava%C3%A7%C3%A3o-local";
+    private String linkConvidar = "https://support.zoom.us/hc/pt-br/articles/201362183-Como-convidar-outras-pessoas-para-ingressar-em-uma-reuni%C3%A3o";
+    private String linkControles = "https://support.zoom.us/hc/pt-br/articles/201362603-Controles-do-anfitri%C3%A3o-em-uma-reuni%C3%A3o";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,4 +40,35 @@ public class ZoomActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+//    LINKS PARA SAIBA COMO
+    public void linkCriar(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkCriar));
+        startActivity(i);
+    }
+    public void linkAgendar(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkAgendar));
+        startActivity(i);
+    }
+    public void linkIngressar(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkIngressar));
+        startActivity(i);
+    }
+    public void linkGravar(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkGravar));
+        startActivity(i);
+    }
+    public void linkConvidar(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkConvidar));
+        startActivity(i);
+    }
+    public void linkControles(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkControles));
+        startActivity(i);
+    }
 }
