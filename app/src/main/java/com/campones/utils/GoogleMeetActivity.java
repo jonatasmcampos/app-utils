00539669071b5a38b631e5297b00f73c837475da
jonatasmcampos <camposjonatas3@gmail.com>
2022-04-12 15:05:52 -0300

    botoes ok

--- app/src/main/java/com/campones/utils/GoogleMeetActivity.java
+++ app/src/main/java/com/campones/utils/GoogleMeetActivity.java
@@ -3,10 +3,15 @@ package com.campones.utils;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 public class GoogleMeetActivity extends AppCompatActivity {
+
+    private String urlTreinamento = "https://edu.google.com/intl/ALL_br/teacher-center/training/?modal_active=none";
+    private String urlPDF = "https://services.google.com/fh/files/misc/google-meet-toolkit_pt-br.pdf";
+    private String urlCentralAjudaGmeet = "https://support.google.com/meet/?hl=pt-BR#topic=7306097";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,21 @@ public class GoogleMeetActivity extends AppCompatActivity {
 
     public void irParaHome(View view){
         finish();
+    }
+
+    public void btnTreinamento(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(urlTreinamento));
+        startActivity(i);
+    }
+    public void btnPdf(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(urlPDF));
+        startActivity(i);
+    }
+    public void btnCentralAjudaGmeet(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(urlCentralAjudaGmeet));
+        startActivity(i);
     }
 }
--- app/src/main/java/com/campones/utils/MicrosoftTeamsActivity.java
+++ app/src/main/java/com/campones/utils/MicrosoftTeamsActivity.java
@@ -3,10 +3,14 @@ package com.campones.utils;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 public class MicrosoftTeamsActivity extends AppCompatActivity {
+
+    private String urlInfo = "https://support.microsoft.com/pt-br/topic/criando-participando-e-executando-reuni%C3%B5es-utilizando-o-teams-para-aprendizagem-remota-788d730f-2c7e-4761-a059-c9b3fe87adf7";
+    private String urlCentralAjudaMteams = "https://support.microsoft.com/pt-br/teams";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,16 @@ public class MicrosoftTeamsActivity extends AppCompatActivity {
 
     public void irParaHome(View view){
         finish();
+    }
+
+    public void btnInformacoes(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(urlInfo));
+        startActivity(i);
+    }
+    public void btnCentralAjudaMteams(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(urlCentralAjudaMteams));
+        startActivity(i);
     }
 }
