b6d753002f88bcbb23234cb5fbf1767dfbfdb8dd
jonatasmcampos <camposjonatas3@gmail.com>
2022-04-13 08:55:00 -0300

    activities ok

--- app/src/main/java/com/campones/utils/GoogleMeetActivity.java
+++ app/src/main/java/com/campones/utils/GoogleMeetActivity.java
@@ -13,6 +13,12 @@ public class GoogleMeetActivity extends AppCompatActivity {
     private String urlPDF = "https://services.google.com/fh/files/misc/google-meet-toolkit_pt-br.pdf";
     private String urlCentralAjudaGmeet = "https://support.google.com/meet/?hl=pt-BR#topic=7306097";
 
+    private String linkCriarGmeet = "https://support.google.com/meet/answer/9302870?hl=pt-BR&co=GENIE.Platform%3DAndroid";
+    private String linkIngressarGmeet = "https://support.google.com/meet/answer/9303069?hl=pt-BR&ref_topic=7192926";
+    private String linkGravarGmeet = "https://support.google.com/meet/answer/9308681?hl=pt-BR";
+    private String linkConvidarGmeet = "https://support.google.com/meet/answer/9303164?hl=pt-BR&ref_topic=7192926";
+    private String linkCompartilharGmeet = "https://support.google.com/meet/answer/9308856?hl=pt-BR&co=GENIE.Platform%3DAndroid";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +42,33 @@ public class GoogleMeetActivity extends AppCompatActivity {
     public void btnCentralAjudaGmeet(View view){
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(urlCentralAjudaGmeet));
+        startActivity(i);
+    }
+
+
+    public void linkCriarGmeet(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkCriarGmeet));
+        startActivity(i);
+    }
+    public void linkIngressarGmeet(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkIngressarGmeet));
+        startActivity(i);
+    }
+    public void linkGravarGmeet(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkGravarGmeet));
+        startActivity(i);
+    }
+    public void linkConvidarGmeet(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkConvidarGmeet));
+        startActivity(i);
+    }
+    public void linkCompartilharGmeet(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkCompartilharGmeet));
         startActivity(i);
     }
 }
--- app/src/main/java/com/campones/utils/MicrosoftTeamsActivity.java
+++ app/src/main/java/com/campones/utils/MicrosoftTeamsActivity.java
@@ -12,6 +12,13 @@ public class MicrosoftTeamsActivity extends AppCompatActivity {
     private String urlInfo = "https://support.microsoft.com/pt-br/topic/criando-participando-e-executando-reuni%C3%B5es-utilizando-o-teams-para-aprendizagem-remota-788d730f-2c7e-4761-a059-c9b3fe87adf7";
     private String urlCentralAjudaMteams = "https://support.microsoft.com/pt-br/teams";
 
+    private String linkAgendarMteams = "https://support.microsoft.com/pt-br/office/agendar-uma-reuni%C3%A3o-no-microsoft-teams-943507a9-8583-4c58-b5d2-8ec8265e04e5";
+    private String linkIngressarMteams = "https://support.microsoft.com/pt-br/office/ingressar-em-uma-reuni%C3%A3o-no-teams-1613bb53-f3fa-431e-85a9-d6a91e3468c9";
+    private String linkGravarMteams = "https://support.microsoft.com/pt-pt/office/gravar-uma-reuni%C3%A3o-no-teams-34dfbe7f-b07d-4a27-b4c6-de62f1348c24?ui=pt-pt&rs=pt-pt&ad=pt";
+    private String linkConvidarMteams = "https://support.microsoft.com/pt-br/office/convidar-pessoas-para-uma-reuni%C3%A3o-no-teams-4bc5981c-446e-4e93-866a-d757466b556a";
+    private String linkCompartilharTelaMteams = "https://support.microsoft.com/pt-br/office/compartilhar-conte%C3%BAdo-em-uma-reuni%C3%A3o-no-teams-fcc2bf59-aecd-4481-8f99-ce55dd836ce8";
+    private String linkCompartilharSomMteams = "https://support.microsoft.com/pt-br/office/compartilhar-som-do-seu-computador-em-uma-reuni%C3%A3o-teams-ou-evento-ao-vivo-dddede9f-e3d0-4330-873a-fa061a0d8e3b";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,38 @@ public class MicrosoftTeamsActivity extends AppCompatActivity {
     public void btnCentralAjudaMteams(View view){
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(urlCentralAjudaMteams));
+        startActivity(i);
+    }
+
+
+    public void linkAgendarMteams(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkAgendarMteams));
+        startActivity(i);
+    }
+    public void linkIngressarMteams(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkIngressarMteams));
+        startActivity(i);
+    }
+    public void linkGravarMteams(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkGravarMteams));
+        startActivity(i);
+    }
+    public void linkConvidarMteams(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkConvidarMteams));
+        startActivity(i);
+    }
+    public void linkCompartilharTelaMteams(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkCompartilharTelaMteams));
+        startActivity(i);
+    }
+    public void linkCompartilharSomMteams(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(linkCompartilharSomMteams));
         startActivity(i);
     }
 }
