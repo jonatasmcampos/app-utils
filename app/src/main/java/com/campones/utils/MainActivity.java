eb251e01532a7f4ad8942552b935b077f48e9f0c
jonatasmcampos <camposjonatas3@gmail.com>
2022-04-13 09:28:27 -0300

    recyclerview click ok

--- app/src/main/java/com/campones/utils/MainActivity.java
+++ app/src/main/java/com/campones/utils/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.campones.utils.models.Imagem;
 import java.util.ArrayList;
--- app/src/main/java/com/campones/utils/RecyclerViewAdapter.java
+++ app/src/main/java/com/campones/utils/RecyclerViewAdapter.java
@@ -1,9 +1,12 @@
 package com.campones.utils;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +18,13 @@ import java.util.ArrayList;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
 
     private ArrayList<Imagem> listaImagem;
+    private String linksFerramentasGoogle[] = {
+            "https://support.google.com/docs/topic/9046002",
+            "https://support.google.com/docs/topic/9054603",
+            "https://support.google.com/docs/topic/9052835",
+            "https://support.google.com/docs/topic/9055404",
+            "https://support.google.com/mail/#topic=7065107",
+            "https://support.google.com/drive/answer/2424384?hl=pt-BR&ref_topic=14940"};
 
     public RecyclerViewAdapter (ArrayList<Imagem> listaImagem){
         this.listaImagem = listaImagem;
@@ -43,6 +53,19 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         int img = listaImagem.get(position).getImagem();
         holder.imagem.setImageResource(img);
 
+        holder.imagem.setTag(position);
+        holder.imagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int clickPosition = (int) view.getTag();
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(linksFerramentasGoogle[clickPosition]));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                view.getContext().startActivity(i);
+
+            }
+        });
     }
 
     @Override
